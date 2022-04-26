914becb42962b652b77caa1c8d99f283e1095618
Vetrov Yury <888vetrov@gmail.com>
2022-04-26 23:44:03 +0300

    Task_36

---
+++ Task_36/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+void Randomizer(int[] Massive) // создаём функцию для заполнения случайными 
+// числами нового массива 
+{
+    for (int i = 0; i < Massive.Length; i++)
+    {
+       Massive[i] = new Random().Next(-10, 100);
+       System.Console.Write(Massive[i] + " ");
+    }
+}
+void View(int[] Massive) // создаём функцию для определения элементов, стоящих на
+// нечетных позициях в массиве 
+{
+    int result = 0; // для хранения результатов для начала равное нулю
+    for (int i = 0; i < Massive.Length; i++)
+    {
+        if (Massive[i] % 2 != 0) // если остаток от деления на 2 НЕ равен 0, то  
+        
+            {
+            result += Massive[i];
+            }
+            
+    }
+    System.Console.WriteLine();
+    System.Console.Write($"Cумма элементов, стоящих на нечётных позициях равна {result} "); 
+    System.Console.WriteLine();
+    
+}
+
+int[] Massive = new int[20];
+Randomizer(Massive);
+View(Massive);
+
